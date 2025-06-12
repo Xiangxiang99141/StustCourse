30544dbf5b974d1f19c57ca4e68468d193151e4d
DogBrother <roys99141@gmail.com>
2025-06-12 20:42:13 +0800

    update 404 pages

--- src/pages/error/404.jsx
+++ src/pages/error/404.jsx
@@ -1,14 +1,16 @@
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router"
 
 function Error404(){
     return(
-        <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <main class="flex min-h-fit px-6 py-24 sm:py-32 lg:px-8 justify-center items-center">
             <div class="text-center">
-                <p class="text-base font-semibold text-indigo-600">404</p>
-                <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">找不到頁面</h1>
-                <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">抱歉，您找的網頁不存在，您可以使用下面的按鈕來尋求協助</p>
+                <p class="text-base font-semibold text-indigo-200">404</p>
+                <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-100 sm:text-7xl">找不到頁面</h1>
+                <p class="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">抱歉，您找的網頁不存在，您可以使用下面的按鈕來尋求協助</p>
                 <div class="mt-10 flex items-center justify-center gap-x-6">
-                <a href="/" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">回首頁</a>
-                <a href="#" class="text-sm font-semibold text-gray-900">聯絡...沒辦法<span aria-hidden="true">&rarr;</span></a>
+                    <Button><Link to="/">回首頁</Link></Button>
+                    <Button variant="outline">聯絡...沒辦法&rarr;</Button>
                 </div>
             </div>
         </main>
