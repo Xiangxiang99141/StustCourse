b6ec539ffcb575e8986faf831b1e2c99ada52c6b
DogBrother <roys99141@gmail.com>
2025-06-12 20:43:19 +0800

    update Footer page

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,8 +1,9 @@
 
 export default function Footer(){
     return(
-        <footer className="fixed bg-gray-900 h-10 bottom-0 w-full">
-            <p>awdawd</p>
+        <footer className="fixed bg-gray-800 h-10 bottom-0 w-full text-center text-sm/10 flex gap-2 justify-center">
+            <span>Copyright © 2025 XiangXiang99141.</span>
+            <span>All Rights Reserved.</span>
         </footer>
     )
 }
