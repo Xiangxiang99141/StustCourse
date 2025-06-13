1ffb8fe85f0d45d5044c93817e42dd13c7786f07
DogBrother <roys99141@gmail.com>
2025-06-13 21:47:48 +0800

    更新 app 路由

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,31 @@ import { Route, Router, Routes } from "react-router"
 //pages
 import Error404 from './pages/error/404'
 import Home from "./pages/Home"
-import Login from "./pages/Login"
 import Header from "./pages/Header"
 import Footer from "./pages/Footer"
-
+import Course from "./pages/Course"
+import Teachers from "./pages/Teacher"
+import CourseLayout from './layout/CourseLayout'
+import CourseSearch from './pages/CourseSearch'
 
 function App() {
   return (
     <>
       <Header/>
-        <div className="min-h-svh w-10/12 -translate-x-1/2 left-1/2 fixed pt-3 shadow-amber-100">
-          <Routes>
-            <Route path="/" element=<Home/> />
-            <Route path="/course" element=<Login/> />
-            <Route path="*" element=<Error404/> />
-          </Routes>
-        </div>
+      <main className="flex-grow container mx-auto pt-3 px-4 bottom-14">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          {/* Course layout + nested pages */}
+          <Route path="/course" element={<CourseLayout />}>
+            <Route path="search/:year/:semester" element={<CourseSearch />} />
+            <Route path=":code" element={<Course />} />
+          </Route>
+          
+          <Route path="/teachers" element={<Teachers/>} />
+          
+          <Route path="*" element={<Error404/>} />
+        </Routes>
+      </main>
       <Footer/>
     </>
   )
