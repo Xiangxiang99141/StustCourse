74d46d7fbcf7a05bceb477654b1f423261793462
DogBrother <roys99141@gmail.com>
2025-06-13 21:46:24 +0800

    更新 header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -6,17 +6,17 @@ export default function Header(){
     const [isOpen,setIsOpen] = useState(false);
     return(
         <>
-            <header className="bg-gray-800 border-b-gray-500 border-b-2 sticky">
+            <header className="bg-gray-800 border-b-gray-500 border-b-2 sticky top-0 w-full z-10">
                 <div className="container mx-auto h-14 flex justify-between items-center ps-3 pe-3">
                     <Button variant="outline" className="border-dashed me-auto">
                         <Link to="/">STUST Course</Link>
                     </Button>
-                    <nav className={`bg-gray-800 absolute flex flex-col w-full left-0 top-14 max-h-fix justify-center items-center z-10 ${isOpen?'max-h-fix py-4 ':'max-h-0'} overflow-hidden ease-in-out transition-all duration-75 md:relative md:w-fit md:flex-row md:py-0 md:max-h-full`}>
+                    <nav className={`bg-gray-800 absolute flex flex-col w-full left-0 top-14 max-h-fix justify-center items-center z-10 ${isOpen?'max-h-fix py-4 ':'max-h-0'} overflow-hidden ease-in-out transition-all duration-75 md:static md:w-fit md:flex-row md:py-0 md:max-h-full`}>
                         <Button variant="link" className="w-full md:w-fit">
-                            <Link to="/courses">查課程</Link>
+                            <Link to="/course">查課程</Link>
                         </Button>
                         <Button variant="link" className="w-full md:w-fit">
-                            <Link to="/teachers">查老師</Link>
+                            <Link to="/teacher">查老師</Link>
                         </Button>
                         <Button variant="link" className="w-full md:w-fit">
                             <Link to="/about">關於作者</Link>
