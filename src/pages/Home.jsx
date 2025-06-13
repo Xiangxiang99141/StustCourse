d5a8053f3487064e6cdd530586b58d5b14ef06b0
DogBrother <roys99141@gmail.com>
2025-06-13 16:06:11 +0800

    更新 首頁大致完成

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,62 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { useState } from "react"
 import 'tailwindcss'
+import infos from '@/assets/info.json'
 
 function Home(){
+    const [classes,setClasses] = useState(25)
+    const [courses,setCourses] = useState(3700)
     return(
-        <Button></Button>
+        <main className="me-4 ms-4">
+            <div className="mb-4 mt-4">
+                <div className="h-14">
+                    <h1 className="text-center text-3xl/10 font-bold ">課程科系大綱</h1>
+                </div>
+                <Card className="bg-gray-800 border-gray-100/15 border px-3">
+                    <div className="grid grid-cols-2 gap-4 place-items-center">
+                        <div>
+                            <span className="font-bold text-2xl pe-1">{classes}</span>
+                            <span className="font-bold text-lg">個科系</span>
+                        </div>
+                        <div>
+                            <span className="font-bold text-2xl pe-1">{courses}+</span>
+                            <span className="font-bold text-lg">個科目</span>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+            <div>
+                <div className="h-14">
+                    <h1 className="text-center text-3xl/10 font-bold ">選課時間與流程</h1>
+                </div>
+                <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
+                { infos.map(info=>{
+                    return(<Card className="bg-gray-800 border-gray-100/15 border px-3" key={info["title"]} >
+                        <CardTitle>
+                            <h3 className="text-xl text-center pb-1">{info["title"]}</h3>
+                            <p className="flex justify-center items-center gap-x-2">
+                                <span className="flex gap-x-2">
+                                    <span className="self-end text-sm text-gray-100/55">{info["time"][0]}</span>
+                                    <span className="text-2xl">{info["date"][0]}</span>
+                                </span>
+                                <span className="text-lg">~</span>
+                                <span className="flex gap-x-2">
+                                    <span className="text-2xl">{info["date"][1]}</span>
+                                    <span className="self-end text-sm text-gray-100/55">{info["time"][1]}
+                                    </span>
+                                </span>
+                            </p>
+                        </CardTitle>
+                        <CardContent>
+                            <p className="pt-2 border-t border-gray-100/15">在第一階段選課時，是以抽籤為主，同學們只要在選課系統中選擇課程。</p>
+                        </CardContent>
+                    </Card>)
+                })}
+                </div>
+            </div>
+        </main>
     )
 }
 
