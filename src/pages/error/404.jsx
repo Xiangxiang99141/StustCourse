b6d20644212313a563c6539ed542d02355675444
DogBrother <roys99141@gmail.com>
2025-06-13 12:30:30 +0800

    修正 class 改為className

--- src/pages/error/404.jsx
+++ src/pages/error/404.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router"
 
 function Error404(){
     return(
-        <main class="flex min-h-fit px-6 py-24 sm:py-32 lg:px-8 justify-center items-center">
-            <div class="text-center">
-                <p class="text-base font-semibold text-indigo-200">404</p>
-                <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-100 sm:text-7xl">找不到頁面</h1>
-                <p class="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">抱歉，您找的網頁不存在，您可以使用下面的按鈕來尋求協助</p>
-                <div class="mt-10 flex items-center justify-center gap-x-6">
+        <main className="flex min-h-fit px-6 py-24 sm:py-32 lg:px-8 justify-center items-center">
+            <div className="text-center">
+                <p className="text-base font-semibold text-indigo-200">404</p>
+                <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-100 sm:text-7xl">找不到頁面</h1>
+                <p className="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">抱歉，您找的網頁不存在，您可以使用下面的按鈕來尋求協助</p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
                     <Button><Link to="/">回首頁</Link></Button>
                     <Button variant="outline">聯絡...沒辦法&rarr;</Button>
                 </div>
