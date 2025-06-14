d02971429624fab3806df141742c89330276d02d
DogBrother <roys99141@gmail.com>
2025-06-15 03:47:43 +0800

    更新 article內連結路徑

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router";
+import { Link, useParams } from "react-router";
 
 export default function Article({code,name,credit,type,instructors,classes,schedule}){
+    const {year,semester} = useParams();
     return(
         <article className="grid bg-white border-black/15 dark:bg-gray-800 dark:border-gray-100/15 relative rounded-2xl border px-4 pb-2 grid-cols-[1.3fr_1fr_1fr] grid-rows-3 gap-x-1.5 gap-y-2 md:grid-cols-[1.5fr_1fr_1fr]">
-                <Link to={code} className="grid col-span-full row-span-full grid-cols-[1.3fr_1fr_1fr] grid-rows-3 gap-x-1.5 gap-y-2 md:grid-cols-[1.5fr_1fr_1fr]">
+                <Link to={`/course/${year}/${semester}/${code}`} className="grid col-span-full row-span-full grid-cols-[1.3fr_1fr_1fr] grid-rows-3 gap-x-1.5 gap-y-2 md:grid-cols-[1.5fr_1fr_1fr]">
                     <h2 className="overflow-hidden text-ellipsis whitespace-nowrap col-span-full border-b border-black/15 py-2 font-bold dark:border-gray-100/15">{name}</h2>
                     <p className="overflow-hidden text-ellipsis whitespace-nowrap before:block before:text-xs before:text-black/55 dark:before:text-gray-100/55 text-center text-sm before:content-['學分']">{credit}</p>
                     <p className="overflow-hidden text-ellipsis whitespace-nowrap before:block before:text-xs before:text-black/55 dark:before:text-gray-100/55 text-center text-sm before:content-['類型']">{type}</p>
