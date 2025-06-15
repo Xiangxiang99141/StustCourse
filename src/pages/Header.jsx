17f34440cabe12a824cf7613e1ecbb9203596af7
DogBrother <roys99141@gmail.com>
2025-06-15 13:20:53 +0800

    更新 header 版面佈局

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -12,15 +12,15 @@ export default function Header(){
                         <Link to="/">STUST Course</Link>
                     </Button>
                     <nav className={`bg-gray-800 absolute flex flex-col w-full left-0 top-14 max-h-fix justify-center items-center z-10 ${isOpen?'max-h-fix py-4 ':'max-h-0'} overflow-hidden ease-in-out transition-all duration-75 md:static md:w-fit md:flex-row md:py-0 md:max-h-full`}>
-                        <Button variant="link" className="w-full md:w-fit">
-                            <Link to="/course/113/1/search">查課程</Link>
-                        </Button>
-                        <Button variant="link" className="w-full md:w-fit">
-                            <Link to="/course/113/1/instructors">查老師</Link>
-                        </Button>
-                        <Button variant="link" className="w-full md:w-fit">
-                            <Link to="/about">關於作者</Link>
-                        </Button>
+                        <Link to="/course/113/1/search" className="w-full">
+                            <Button variant="link" className="w-full md:w-fit cursor-pointer">查課程</Button>
+                        </Link>
+                        <Link to="/course/113/1/instructors" className="w-full">
+                            <Button variant="link" className="w-full md:w-fit cursor-pointer">查老師</Button>
+                        </Link>
+                        <Link to="/about" className="w-full">
+                            <Button variant="link" className="w-full md:w-fit cursor-pointer">關於作者</Button>
+                        </Link>
                     </nav>
                     <Button className="w-10 h-10 grid place-items-center border border-gray-100/15 rounded-sm md:hidden cursor-pointer hover:bg-gray-700" variant="ghost" onClick={()=>setIsOpen(!isOpen)}>
                         <span data-v-3afe5ad9="" className="sr-only">打開選單</span>
