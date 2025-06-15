61dcb9c45370bf02df93eba2048038b02a104635
DogBrother <roys99141@gmail.com>
2025-06-15 21:56:07 +0800

    新增 各學期超連結

--- src/pages/CourseHome.jsx
+++ src/pages/CourseHome.jsx
@@ -1,11 +1,23 @@
-import { Link } from "react-router";
+import { setClass } from "@/store/module/filterSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useParams } from "react-router";
+
 
 export default function CourseHome(){
+    const classesQuery = useParams().classes
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        if(classesQuery!=undefined){
+            dispatch(setClass(classesQuery))
+        }
+    },[])
     return(
-        <div className="grid grid-cols-3 p-3 rounded-lg bg-gray-800">
-            <Link to={"113/1"} className="text-3xl text-center h-10 font-bold hover:border-b-1 border-white">113上學期</Link>
-            <Link to={"113/2"} className="text-3xl text-center h-10 font-bold hover:border-b-1 border-white">113下學期</Link>
-            <Link to={"113/3"} className="text-3xl text-center h-10 font-bold hover:border-b-1 border-white">113暑修</Link>
+        
+        <div className="grid grid-cols-1 md:grid-cols-3 p-3 gap-4 rounded-lg bg-gray-800">
+            <Link to={"/course/113/1/search"} className="text-3xl text-center h-10 font-bold hover:border-b-1 border-white">113上學期</Link>
+            <Link to={"/course/113/2/search"} className="text-3xl text-center h-10 font-bold hover:border-b-1 border-white">113下學期</Link>
+            <Link to={"/course/113/3/search"} className="text-3xl text-center h-10 font-bold hover:border-b-1 border-white">113暑修</Link>
         </div>
     )
 }
